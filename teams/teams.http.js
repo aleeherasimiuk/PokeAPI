a71eca2c5063246015c5c12029e278b864e61280
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-07-31 23:26:38 -0300

    Using await instead of axios.then().catch()

--- teams/teams.http.js
+++ teams/teams.http.js
@@ -5,37 +5,35 @@ const usersController = require('../users/users.controller.js');
 
 const getTeamFromUser = async (request, response) => {
   let userID = request.user.userId;
-    let user = usersController.getUser(userID);
-    let team = await teamsController.getTeamOfUser(userID);
-    response.status(200).json({
-      trainer: user.username,
-      team: team
-    })
+  let user = usersController.getUser(userID);
+  let team = await teamsController.getTeamOfUser(userID);
+  response.status(200).json({
+    trainer: user.username,
+    team: team
+  })
 }
 
 const setTeamToUser = (request, response) => {
   teamsController.setTeam(request.user.userId, request.body.team);
-    response.sendStatus(200);
+  response.sendStatus(200);
 }
 
-const addPokemon = (request, response) => {
+const addPokemon = async (request, response) => {
   let pokemonName = request.body.name;
-  axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`)
-    .then(async (pokeApiResponse) => {
-      let id = pokeApiResponse.data.id;
-      let pokemon = {
-        name: pokemonName,
-        pokedexNumber: id
-      }
-      try {
-        await teamsController.addPokemon(request.user.userId, pokemon);
-        response.sendStatus(201).json(pokemon);
-      } catch (error) {
-        response.status(400).json({ message: "You have already 6 pokemon" });
-      }
-    }).catch((error) => {
-      response.sendStatus(400);
-    })
+  let pokeApiResponse = 
+    await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`)
+  let id = pokeApiResponse.data.id;
+  let pokemon = {
+    name: pokemonName,
+    pokedexNumber: id
+  }
+  try {
+    await teamsController.addPokemon(request.user.userId, pokemon);
+    response.sendStatus(201).json(pokemon);
+  } catch (error) {
+    response.status(400).json({ message: "You have already 6 pokemon" });
+  }
+
 }
 
 const deletePokemon = (request, response) => {
